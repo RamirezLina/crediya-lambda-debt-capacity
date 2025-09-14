1bf0ac7cfec4887e4ad9b310b0d83a03b12a2409
Lina María <linaramirezm11@gmail.com>
2025-09-14 14:36:55 -0500

    Add project structure

---
+++ src/config.mjs
@@ -0,0 +1,4 @@
+export const CONFIG = {
+  fromEmail: process.env.FROM_EMAIL,
+  subject: process.env.SUBJECT ?? "Validación automática de tu solicitud de crédito"
+};
---
+++ src/services/mailService.mjs
@@ -0,0 +1,22 @@
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+import { CONFIG } from "../config.mjs";
+
+
+export async function sendHtmlEmail(to, html) {
+  const params = {
+    Source: CONFIG.fromEmail,
+    Destination: { ToAddresses: [to] },
+    Message: {
+      Subject: {
+        Data: CONFIG.region
+      },
+      Body: {
+        Html: {
+          Data: html
+        }
+      }
+    }
+  };
+  return ses.send(new SendEmailCommand(params));
+}
+
