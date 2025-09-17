2440fd5284e5837381bc7db7604db51f1ad9374a
LinaRamirez <lina.ramirez@sofka.com.co>
2025-09-16 22:49:16 -0500

    Add queueUrl and region to CONFIG for enhanced configuration management

--- src/config.mjs
+++ src/config.mjs
@@ -1,4 +1,6 @@
 export const CONFIG = {
   fromEmail: process.env.FROM_EMAIL,
-  subject: process.env.SUBJECT ?? "Validación automática de tu solicitud de crédito"
+  subject: process.env.SUBJECT ?? "Validación automática de tu solicitud de crédito",
+  queueUrl: process.env.RESULT_QUEUE_URL,
+  region: process.env.AWS_REGION || "us-east-2"
 };
